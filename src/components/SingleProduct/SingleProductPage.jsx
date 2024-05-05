82e2f5fd41f87847a5f0c06dcbd5f9d5a9e00915
tejal <tejal@enstead.com>
2024-05-05 18:53:40 +0530

    Single Product detail design

--- src/components/SingleProduct/SingleProductPage.jsx
+++ src/components/SingleProduct/SingleProductPage.jsx
@@ -6,7 +6,7 @@ const product = {
   title: "Product Title",
   description:
     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime aliquid rerum a? Fugiat soluta facilis deleniti voluptatibus ab architecto dolores a, vero, beatae veniam error doloribus quia laudantium? Error fuga consequuntur quia accusantium? Consequatur modi laboriosam saepe culpa, ab atque.",
-  price: 9.99,
+  price: 9.9911,
   images: [
     "https://via.placeholder.com/500x500?text=Product+Image+1",
     "https://via.placeholder.com/500x500?text=Product+Image+2",
@@ -37,7 +37,20 @@ const SingleProductPage = () => {
           className="single_product_display"
         />
       </div>
-      <div className="align_center single_product_details"></div>
+      <div className="single_product_details">
+        <h1 className="single_product_title">{product.title}</h1>
+        <p className="single_product_description">{product.description}</p>
+        <p className="single_product_price">${product.price.toFixed(2)}</p>
+        <h2 className="quantity_title">Quantity:</h2>
+        <div className="align_center quantity_input">
+          <button className="quantity_input_button" disabled>
+            -
+          </button>
+          <p className="quantity_input_count">1</p>
+          <button className="quantity_input_button">+ </button>
+        </div>
+        <button className="search_button add_cart">Add to Cart</button>
+      </div>
     </section>
   );
 };
