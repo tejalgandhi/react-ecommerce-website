7245e31f1fb0ab7703241c665eea7977105ce1a6
tejal <tejal@enstead.com>
2024-05-13 19:03:27 +0530

    single product api and loading change

---
+++ src/components/Common/Loading.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Loading.css";
+import loading from "../../assets/loading.gif";
+
+const Loading = () => {
+  return <img src={loading} className="loading align_center"></img>;
+};
+
+export default Loading;
--- src/components/SingleProduct/SingleProductPage.jsx
+++ src/components/SingleProduct/SingleProductPage.jsx
@@ -4,6 +4,8 @@ import { useParams } from "react-router-dom";
 import apiClient from "../../../utils/api-client";
 import "./SingleProductPage.css";
 import QuantityInput from "./QuantityInput";
+import useData from "../../hooks/useData";
+import Loading from "../Common/Loading";
 
 const products = {
   id: 1,
@@ -22,53 +24,59 @@ const products = {
 
 const SingleProductPage = () => {
   const { id } = useParams();
-  const [product, setProduct] = useState({});
+  // const [product, setProduct] = useState({});
   const [selectedImage, setSelectedImage] = useState(0);
-  useEffect(() => {
-    apiClient
-      .get(`product/${id}`)
-      .then((res) => setProduct(res.data.data))
-      .catch((err) => setError(err.message));
-  }, []);
+  const { data: product, error, isLoading } = useData(`/product/${id}`, []);
+  //   useEffect(() => {
 
-  if (product && Object.keys(product).length > 0) {
-    return (
-      <section className="align_center single_product">
-        <div className="align_center">
-          <div className="single_product_thumbnails">
-            {console.log(product.images)}
-            {product.images.map((image, index) => (
-              <img
-                src={image}
-                alt={product.title}
-                className={selectedImage == index ? "selected_image" : ""}
-                onClick={() => setSelectedImage(index)}
-              />
-            ))}
+  //   apiClient
+  //     .get(`product/${id}`)
+  //     .then((res) => setProduct(res.data.data))
+  //     .catch((err) => setError(err.message));
+  // }, []);
+
+  // Render a message or fallback content if product is empty or undefined
+  return (
+    <>
+      {isLoading && <Loading />}
+      {!isLoading && !product && (
+        <p className="align_center margin_center">No product available</p>
+      )}
+      {!isLoading && product && Object.keys(product).length > 0 && (
+        <section className="align_center single_product">
+          <div className="align_center">
+            <div className="single_product_thumbnails">
+              {console.log(product.images)}
+              {product.images.map((image, index) => (
+                <img
+                  src={image}
+                  alt={product.title}
+                  className={selectedImage == index ? "selected_image" : ""}
+                  onClick={() => setSelectedImage(index)}
+                />
+              ))}
+            </div>
+            {/* {images.map((image, index) => (
+                   <img key={index} src={`${backendStorageUrl}/${image}`} alt={`Image ${index + 1}`} />
+               ))} */}
+            <img
+              src={product.images[selectedImage]}
+              alt={product.title}
+              className="single_product_display"
+            />
           </div>
-          {/* {images.map((image, index) => (
-                <img key={index} src={`${backendStorageUrl}/${image}`} alt={`Image ${index + 1}`} />
-            ))} */}
-          <img
-            src={product.images[selectedImage]}
-            alt={product.title}
-            className="single_product_display"
-          />
-        </div>
-        <div className="single_product_details">
-          <h1 className="single_product_title">{product.title}</h1>
-          <p className="single_product_description">{product.description}</p>
-          <p className="single_product_price">${product.price.toFixed(2)}</p>
-          <h2 className="quantity_title">Quantity:</h2>
-          <QuantityInput />
-          <button className="search_button add_cart">Add to Cart</button>
-        </div>
-      </section>
-    );
-  } else {
-    // Render a message or fallback content if product is empty or undefined
-    return <p>No product available</p>;
-  }
+          <div className="single_product_details">
+            <h1 className="single_product_title">{product.title}</h1>
+            <p className="single_product_description">{product.description}</p>
+            <p className="single_product_price">${product.price.toFixed(2)}</p>
+            <h2 className="quantity_title">Quantity:</h2>
+            <QuantityInput />
+            <button className="search_button add_cart">Add to Cart</button>
+          </div>
+        </section>
+      )}
+    </>
+  );
 };
 
 export default SingleProductPage;
